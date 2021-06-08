92054ddde5e0b383f3644dec38bbfd04a0bea276
Anibrata Saha <broto76@gmail.com>
2021-06-08 13:00:10 +0530

    Added navigation drawer implementation to MainActivity.kt

--- app/src/main/java/com/broto/projtracker/activities/MainActivity.kt
+++ app/src/main/java/com/broto/projtracker/activities/MainActivity.kt
@@ -1,12 +1,79 @@
 package com.broto.projtracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
 import com.broto.projtracker.R
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBar()
+        main_nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar_main_activity)
+
+        toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        toolbar_main_activity.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        if (main_drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            main_drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            main_drawer_layout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (main_drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            main_drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_menu_my_profile -> {
+                Toast.makeText(
+                    this@MainActivity,
+                    "My Profile",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.nav_menu_signout -> {
+                Log.d(TAG, "Sign Out User: ${FirebaseAuth.getInstance().currentUser?.uid}")
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this@MainActivity, IntroActivity::class.java)
+                intent.addFlags(
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP or
+                            Intent.FLAG_ACTIVITY_NEW_TASK
+                )
+                startActivity(intent)
+                finish()
+            }
+        }
+        if (main_drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            main_drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        return true
     }
 }
